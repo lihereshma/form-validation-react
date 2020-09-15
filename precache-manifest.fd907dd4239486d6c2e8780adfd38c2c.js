cbfbbbafde261def0f65e89f8ca84a9682b02430
lihereshma <reshmalihe00068@gmail.com>
2020-09-15 12:40:56 +0530

    Updates

---
+++ precache-manifest.fd907dd4239486d6c2e8780adfd38c2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46021177d2641b130e7008fbbdbdd6b",
+    "url": "/form-validation-react/index.html"
+  },
+  {
+    "revision": "c14a26053be5a801d548",
+    "url": "/form-validation-react/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "d8c040be0998838f1069",
+    "url": "/form-validation-react/static/css/main.5676d006.chunk.css"
+  },
+  {
+    "revision": "c14a26053be5a801d548",
+    "url": "/form-validation-react/static/js/2.bc04aafa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/form-validation-react/static/js/2.bc04aafa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c040be0998838f1069",
+    "url": "/form-validation-react/static/js/main.a32d3289.chunk.js"
+  },
+  {
+    "revision": "2388dcbcae454b99ddc7",
+    "url": "/form-validation-react/static/js/runtime-main.6f531a0b.js"
+  }
+]);
